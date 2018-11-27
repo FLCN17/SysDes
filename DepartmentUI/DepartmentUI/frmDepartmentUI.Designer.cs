9f9cd905c27114260382bb90b61e385bdcbfc059
NBakes <45102299+NBakes@users.noreply.github.com>
2018-11-26 23:17:49 -0500

    Add files via upload
    Department and Student UI

---
+++ DepartmentUI/DepartmentUI/frmDepartmentUI.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace UI
+{
+    partial class frmUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lstCourse = new System.Windows.Forms.ListBox();
+            this.lstCert = new System.Windows.Forms.ListBox();
+            this.lstMajor = new System.Windows.Forms.ListBox();
+            this.lstSelections = new System.Windows.Forms.ListBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select Course:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(253, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Select Certification:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(456, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Select Major:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(47, 236);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Your Selections:";
+            // 
+            // lstCourse
+            // 
+            this.lstCourse.FormattingEnabled = true;
+            this.lstCourse.ItemHeight = 15;
+            this.lstCourse.Location = new System.Drawing.Point(50, 60);
+            this.lstCourse.Name = "lstCourse";
+            this.lstCourse.Size = new System.Drawing.Size(160, 139);
+            this.lstCourse.TabIndex = 9;
+            // 
+            // lstCert
+            // 
+            this.lstCert.FormattingEnabled = true;
+            this.lstCert.ItemHeight = 15;
+            this.lstCert.Location = new System.Drawing.Point(256, 60);
+            this.lstCert.Name = "lstCert";
+            this.lstCert.Size = new System.Drawing.Size(160, 139);
+            this.lstCert.TabIndex = 10;
+            // 
+            // lstMajor
+            // 
+            this.lstMajor.FormattingEnabled = true;
+            this.lstMajor.ItemHeight = 15;
+            this.lstMajor.Location = new System.Drawing.Point(459, 60);
+            this.lstMajor.Name = "lstMajor";
+            this.lstMajor.Size = new System.Drawing.Size(160, 139);
+            this.lstMajor.TabIndex = 11;
+            this.lstMajor.SelectedIndexChanged += new System.EventHandler(this.lstMajor_SelectedIndexChanged);
+            // 
+            // lstSelections
+            // 
+            this.lstSelections.FormattingEnabled = true;
+            this.lstSelections.ItemHeight = 15;
+            this.lstSelections.Location = new System.Drawing.Point(50, 254);
+            this.lstSelections.Name = "lstSelections";
+            this.lstSelections.Size = new System.Drawing.Size(363, 169);
+            this.lstSelections.TabIndex = 12;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(459, 325);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(160, 47);
+            this.btnReset.TabIndex = 13;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(459, 254);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(160, 49);
+            this.btnSearch.TabIndex = 14;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(495, 398);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(84, 25);
+            this.btnExit.TabIndex = 15;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // frmUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 464);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.lstSelections);
+            this.Controls.Add(this.lstMajor);
+            this.Controls.Add(this.lstCert);
+            this.Controls.Add(this.lstCourse);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "frmUI";
+            this.Text = "Reporting System";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox lstCourse;
+        private System.Windows.Forms.ListBox lstCert;
+        private System.Windows.Forms.ListBox lstMajor;
+        private System.Windows.Forms.ListBox lstSelections;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
